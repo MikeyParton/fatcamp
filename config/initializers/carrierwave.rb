123c9e119a55410cff0e7f2287387987f165f386
Mike Parton <mikeparton@live.com.au>
2016-11-22 13:27:53 -0300

    Fix env variables

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -2,11 +2,11 @@ CarrierWave.configure do |config|
 	config.fog_provider = 'fog/google'  
 	config.fog_credentials = {
 
-	    :provider                         => 'fatcamp',
+	    :provider                         => "Google",
 	    :google_storage_access_key_id     => "GOOGYYV3CILLK22M2L5X",
 	    :google_storage_secret_access_key => "51UhrUcJDjs2hAbH0RAB4s4H3BIMGXZpOezXsWST"
 
 	    }
 
-	    config.fog_directory = ENV["google_storage_bucket_name"]
+	    config.fog_directory = "fatcamp"
 end
