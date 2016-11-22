d001edb9fb1a783ca26d660e00cea5dc4a823e32
Mike Parton <mikeparton@live.com.au>
2016-11-22 13:24:54 -0300

    Test env variables

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -2,9 +2,9 @@ CarrierWave.configure do |config|
 	config.fog_provider = 'fog/google'  
 	config.fog_credentials = {
 
-	    :provider                         => 'Google',
-	    :google_storage_access_key_id     => ENV["google_storage_access_key_id"],
-	    :google_storage_secret_access_key => ENV["google_storage_secret_access_key"]
+	    :provider                         => 'fatcamp',
+	    :google_storage_access_key_id     => "GOOGYYV3CILLK22M2L5X",
+	    :google_storage_secret_access_key => "51UhrUcJDjs2hAbH0RAB4s4H3BIMGXZpOezXsWST"
 
 	    }
 
