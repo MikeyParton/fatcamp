1f97038680f80383f6c927fb61b9a6d339d2ccef
Mike Parton <mikeparton@live.com.au>
2016-11-23 14:29:32 -0300

    fog credentials test

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -2,11 +2,12 @@ CarrierWave.configure do |config|
 	config.fog_provider = 'fog/google'  
 	config.fog_credentials = {
 
-	    :provider                         => "Google",
-	    :google_storage_access_key_id     => ENV["google_storage_access_key_id"],
-	    :google_storage_secret_access_key => ENV["google_storage_secret_access_key"]
+    :provider                         => "Google",
+    :google_storage_access_key_id     => ENV["GOOGLE_STORAGE_ACCESS_KEY_ID" ],
+    :google_storage_secret_access_key => ENV["GOOGLE_STORAGE_SECRET_ACCESS_KEY" ]
 
-	    }
+    }
 
-	    config.fog_directory = ENV["google_storage_bucket_name"]
+    config.fog_directory = ENV["GOOGLE_STORAGE_BUCKET_NAME" ]
+    
 end
