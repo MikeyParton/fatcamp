698d6de62bba94fd30c3c345444ade19207fd629
Mike Parton <mikeparton@live.com.au>
2016-11-23 15:03:44 -0300

    secrets test

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,7 +3,7 @@ CarrierWave.configure do |config|
 	config.fog_credentials = {
 
 	    :provider                         => "Google",
-	    :google_storage_access_key_id     => "GOOGYYV3CILLK22M2L5X",
+	    :google_storage_access_key_id     => Rails.application.secrets.google_storage_access_key_id,
 	    :google_storage_secret_access_key => "51UhrUcJDjs2hAbH0RAB4s4H3BIMGXZpOezXsWST"
 
 	    }
