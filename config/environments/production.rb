af320e71ee472898861f85f8e6af70048e4320f7
Mike Parton <mikeparton@live.com.au>
2016-11-24 17:44:26 -0300

    fix mailer syntax

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -94,7 +94,7 @@ Rails.application.configure do
     :user_name            => "mikesfatcamp@gmail.com",
     :password             => "1Dardymob",
     :authentication       => "plain",
-    :enable_starttls_auto => true
+    :enable_starttls_auto => true,
     :openssl_verify_mode  => 'none'
   }
 end
